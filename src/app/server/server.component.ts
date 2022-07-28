5f7dafedd4d95b4b76cc276dcd848398b7de4a04
ZhanatShengelbay <10541107@polimi.it>
2022-07-28 12:52:59 +0200

    2 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -14,6 +14,7 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   allowNewServer = false;
   serverStatus = 'No server was created';
+  serverName = 'test';
 
   constructor() {
     setTimeout(() => {
@@ -24,7 +25,7 @@ export class ServerComponent {
   onCreationServer() {
     this.serverStatus = 'Server was created!';
   }
-  onUpdateServer(event: any){
+  onUpdateServer(event: any) {
     console.log(event);
   }
 
