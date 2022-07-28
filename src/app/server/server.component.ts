3e08875db380c075b85d20aeaac3839ad321b8b6
ZhanatShengelbay <10541107@polimi.it>
2022-07-28 11:32:07 +0200

    3 files modified

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -11,4 +11,22 @@ import { Component } from '@angular/core';
     `,
   ],
 })
-export class ServerComponent {}
+export class ServerComponent {
+  allowNewServer = false;
+  serverStatus = 'No server was created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreationServer() {
+    this.serverStatus = 'Server was created!';
+  }
+  onUpdateServer(event: any){
+    console.log(event);
+  }
+
+  ngOnInit() {}
+}
